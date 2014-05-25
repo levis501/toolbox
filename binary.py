8de92b0ea5d1057e31099868bac024c3dd773c09
Steven Levis <levis501@yahoo.com>
2014-05-24 20:03:17 -0700

    Fixed BitwiseData.__repr__ to agree with new order of init params

--- binary.py
+++ binary.py
@@ -182,7 +182,7 @@ class BitwiseData:
   def __hash__(self):
     return self.value % ((1<<31)-1)
   def __repr__(self):
-    return "BitwiseData(0x%X,%d)" % (self.value, self.count)
+    return "BitwiseData(%d,0x%X)" % (self.count, self.value)
   def __str__(self):
     return self.bitStr()
   def __iter__(self):
