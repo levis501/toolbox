9cda343c822d31e1707fe527800de85eedceceb2
Steven Levis <levis501@yahoo.com>
2022-09-22 19:33:59 -0400

    refactored to fork_then

--- snap_refresh.c
+++ snap_refresh.c
@@ -3,22 +3,36 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main() {
-  //const char *path="/usr/bin/echo";
-  const char *path="/usr/bin/snap";
-  const char *arg1="refresh";
-  
-  pid_t pid = fork();
-  if (pid) {
-    // parent
-    int wstatus;
-    wait(&wstatus);
+
+
+
+void enter_to_continue() {
     printf("Press Enter to continue...\n");
     while (getchar() != '\n') {}
+}
+
+void exec_snap_refresh() {
+  const char *path="/usr/bin/snap";
+  const char *arg1="refresh";
+
+  printf("Refreshing snaps...\n");
+  execlp(path, path, arg1, (char *) NULL);
+}
+
+
+int fork_then(void (*child)(), void (*parent)()) {
+  if (fork()) {
+    int wstatus;
+    wait(&wstatus);
+    parent();
     return wstatus;
   } else {
-    //child
-    printf("Refreshing snaps...\n");
-    return execlp(path, path, arg1, (char *) NULL);
+    child();
   }
+}
+  
+int main() {
+
+  int wstatus = fork_then(exec_snap_refresh, enter_to_continue);
+  return wstatus;
 }
